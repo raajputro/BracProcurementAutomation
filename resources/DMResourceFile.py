af2a490ee2cb5575ae777da68734689893b0f32b
fahmida.akther <fahmida.himu0904@gmail.com>
2025-08-15 20:23:17 +0600

    Order review flow half complete

--- resources/DMResourceFile.py
+++ resources/DMResourceFile.py
@@ -1,24 +1,23 @@
 class TestResources:
     test_url = "https://env28.erp.bracits.net/"
-# <<<<<<< HEAD
+    # <<<<<<< HEAD
     test_user_name = "761"
-# =======
-    test_user_name = "138486" #"197363" # "15370" #
-# >>>>>>> 95b5531cb398e0bbd0eb92335c353f12de67ae3b
+    # =======
+    test_user_name = "138486"  # "197363" # "15370" #
+    # >>>>>>> 95b5531cb398e0bbd0eb92335c353f12de67ae3b
     test_user_pass = "abc123$"
     test_requisition_initiator = "761"
     test_requisition_budget_recommender = "15370"
     test_requisition_approver = "138486"
     test_admin = "137124"
     test_requisition_assignee = "00000761"
-    test_requisition_number="REQ20250004299"
-    test_vendor_name = "Skylark Printers"
+    test_requisition_number = "REQ20250004299"
+    # test_vendor_name = "Skylark Printers"
     test_purchase_approver = "00062076"
     generated_po_number = None
     test_bill_recommender = "00137124"
 
-    #erp_url = "https://env28.erp.bracits.net/"
-
+    # erp_url = "https://env28.erp.bracits.net/"
 
 # class TestResourcesDM:
 #     test_url = "https://qamarketplace.bracits.com/login?ReturnUrl=%2F"
@@ -37,12 +36,12 @@ class TestResources:
 #     approver_order_rejection = "2025/TRN-2469"
 #     test_vendor_send_back_reasons = "vendorSendBackReasons"
 #     review_reasons = "Need to review order"
-    # test_requisition_approver = "138486"
-    # test_admin = "137124"
-    # test_requisition_assignee = "00000761"
-    # test_requisition_number="REQ20250004299"
-    # test_vendor_name = "Skylark Printers"
-    # test_purchase_approver = "00062076"
-    # generated_po_number = None
+# test_requisition_approver = "138486"
+# test_admin = "137124"
+# test_requisition_assignee = "00000761"
+# test_requisition_number="REQ20250004299"
+# test_vendor_name = "Skylark Printers"
+# test_purchase_approver = "00062076"
+# generated_po_number = None
 
-    # erp_url = "https://env28.erp.bracits.net/"
+# erp_url = "https://env28.erp.bracits.net/"
